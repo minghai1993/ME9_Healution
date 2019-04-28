f94f827e5d92d22c4c1fe1f6a2b96e3a472dc33d
Elijah <hsminghai@gmail.com>
2019-04-28 23:57:25 +1000

    security script 2 - Enzo

---
+++ user-detail.sh
@@ -0,0 +1,16 @@
+echo "Starting to run the script..."
+# VARIABLE ASSIGNMENT
+# Show hostname:
+HOST=$(hostname)
+# User executing the script:
+CURRENTUSER=$(whoami)
+# Current date:
+CURRENTDATE=$(date +%F)
+# Host IP address:
+IPADDRESS=$(hostname -I | cut -d ' ' -f1)
+
+# SHOW MESSAGES
+echo "Today is $CURRENTDATE"
+echo "Hostname: $HOST ($IPADDRESS)"
+echo "User info for $CURRENTUSER:"
+grep $CURRENTUSER /etc/passwd
